a9f531867e55ac481df2ef5662c20a60295a615d
sos1819agf <antonio.antoniog.garcia@gmail.com>
2019-03-19 18:38:35 +0000

    basic server

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require("express");
-var bodyParser = require("body-parser");
+//var bodyParser = require("body-parser");
+var path = require("path");
 
 var app = express();
 
-app.use(bodyParser.json());
+app.use("/", express.static(path.join(__dirname, "public")));
+//app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;
 
