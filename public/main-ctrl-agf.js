deb5aeca03079f1c17a0183a2be898978ecee244
sos1819agf <antonio.antoniog.garcia@gmail.com>
2019-04-08 15:58:15 +0000

    e

--- public/main-ctrl-agf.js
+++ public/main-ctrl-agf.js
@@ -9,5 +9,7 @@ app.controller("MainCtrl", ["$scope","$http", function ($scope,$http){
                     $http.get($scope.url).then(function (response){
                         $scope.data = JSON.stringify(response.data,null,2);
                     });
+                    
+                    $http.post($scope.url)
                 }
             }]);
