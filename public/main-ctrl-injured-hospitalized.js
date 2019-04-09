6e6e65f4eac997fa8dedf76539d6063592d99e34
sos1819agf <antonio.antoniog.garcia@gmail.com>
2019-04-09 21:06:46 +0000

    eii

--- public/main-ctrl-injured-hospitalized.js
+++ public/main-ctrl-injured-hospitalized.js
@@ -8,12 +8,13 @@ app.controller("MainCtrl", ["$scope", "$http", function($scope, $http) {
     $scope.sendGet = function() {
         $http.get($scope.url).then(function(response) {
             var res = JSON.stringify(response.data, null, 2);
-            if (response.data.length == 0) {
 
-            }
             $scope.dataResponse = res;
+            $scope.stateCode = response.status;
         }, function(response) {
             $scope.dataResponse = response.status + ", " + response.statusText
+            $scope.stateCode = response.status;
+
         });
     }
 
@@ -36,10 +37,12 @@ app.controller("MainCtrl", ["$scope", "$http", function($scope, $http) {
             $http.post($scope.url, JSON.stringify(data)).then(function(response) {
                 console.log("OK post method");
                 $scope.dataResponse = JSON.stringify(response.data, null, 2);
+                $scope.stateCode = response.status;
             }, function(response) {
                 console.log("Error POST method: Code " + response.status + ", " + response.statusText);
                 console.log("caaaaabron");
                 $scope.dataResponse = "Code: " + response.status + "\n" + response.statusText;
+                $scope.stateCode = response.status;
             });
         }
         else {
@@ -66,9 +69,11 @@ app.controller("MainCtrl", ["$scope", "$http", function($scope, $http) {
             $http.put($scope.url, JSON.stringify(data)).then(function (response) {
                 console.log("OK put method");
                 $scope.dataResponse = JSON.stringify(response.data,null,2);
+                $scope.stateCode = response.status;
             }, function (response) {
                 console.log("Error PUT method: Code "+response.status+", "+response.statusText);
                 $scope.dataResponse="Code: "+response.status+"\n"+response.statusText;
+                $scope.stateCode = response.status;
             });
         }else{
             $scope.dataResponse="Fields required";
@@ -83,8 +88,10 @@ app.controller("MainCtrl", ["$scope", "$http", function($scope, $http) {
                 
             }
             $scope.dataResponse = res;
+            $scope.stateCode = response.status;
         }, function (response) {
-            $scope.dataResponse=response.status+", "+response.statusText
+            $scope.dataResponse=response.status+", "+response.statusText;
+            $scope.stateCode = response.status;
         });
         
     }
