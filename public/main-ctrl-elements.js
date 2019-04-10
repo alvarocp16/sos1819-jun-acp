1534a22c7c3337d292ea03f6bdffa2eedc1ea458
sos1819acp <alvarocp16@gmail.com>
2019-04-10 15:50:45 +0000

    adbs

--- public/main-ctrl-elements.js
+++ public/main-ctrl-elements.js
@@ -5,17 +5,17 @@ app.controller("MainCtrl", ["$scope", "$http", function($scope, $http) {
     console.log("MainCtrl initialized!");
     $scope.url = "/api/v1/elements";
 
-    $scope.sendGet = function(){
-        $http.get($scope.url).then(function(response){
-            var res = JSON.stringify(response.data,null,2);
+    $scope.sendGet = function() {
+        $http.get($scope.url).then(function(response) {
+            var res = JSON.stringify(response.data, null, 2);
             $scope.dataResponse = res;
             $scope.stateCode = response.status;
-        }, function (response) {
-            $scope.dataResponse=response.status+", "+response.statusText;
+        }, function(response) {
+            $scope.dataResponse = response.status + ", " + response.statusText;
             $scope.stateCode = response.status;
         });
     };
-    $scope.sendPost = function(province, year, victims, injurednothospitalizedinaccidents, accidentswithvictims) {
+    $scope.sendPostParam = function(province, year, victims, injurednothospitalizedinaccidents, accidentswithvictims) {
         if (province != 'undefined' &&
             year != 'undefined' &&
             victims != 'undefined'&&
@@ -40,12 +40,32 @@ app.controller("MainCtrl", ["$scope", "$http", function($scope, $http) {
             $scope.dataResponse = "Fields required";
         }
     };
-    $scope.sendPut = function(province, year, victims, injurednothospitalizedinaccidents, accidentswithvictims){
-        if(province != 'undefined' &&
+    $scope.sendPost = function() {
+        var element;
+        if ($scope.dataResponse) {
+            element = $scope.dataResponse;
+        }
+        else {
+            element = {
+                province: $scope.province,
+                year: parseInt($scope.year),
+                victims: $scope.victims,
+                injurednothospitalizedinaccidents: $scope.injurednothospitalizedinaccidents,
+                accidentswithvictims: $scope.accidentswithvictims
+            };
+        }
+        $http.post($scope.url, element).then(function(response) {
+            $scope.stateCode = response.status;
+        }, function(error) {
+            $scope.stateCode = error.status;
+        });
+    };
+    $scope.sendPut = function(province, year, victims, injurednothospitalizedinaccidents, accidentswithvictims) {
+        if (province != 'undefined' &&
             year != 'undefined' &&
-            victims != 'undefined'&&
+            victims != 'undefined' &&
             injurednothospitalizedinaccidents != 'undefined' &&
-            accidentswithvictims != 'undefined'){
+            accidentswithvictims != 'undefined') {
             var data = {
                 province: province,
                 year: parseInt(year),
@@ -53,24 +73,25 @@ app.controller("MainCtrl", ["$scope", "$http", function($scope, $http) {
                 injurednothospitalizedinaccidents: injurednothospitalizedinaccidents,
                 accidentswithvictims: accidentswithvictims
             };
-            $http.put($scope.url, JSON.stringify(data)).then(function (response) {
-                $scope.dataResponse = JSON.stringify(response.data,null,2);
+            $http.put($scope.url, JSON.stringify(data)).then(function(response) {
+                $scope.dataResponse = JSON.stringify(response.data, null, 2);
                 $scope.stateCode = response.status;
-            }, function (response) {
-                $scope.dataResponse=response.status+"\n"+response.statusText;
+            }, function(response) {
+                $scope.dataResponse = response.status + "\n" + response.statusText;
                 $scope.stateCode = response.status;
             });
-        }else{
-            $scope.dataResponse="Fields required";
+        }
+        else {
+            $scope.dataResponse = "Fields required";
         }
     };
-    $scope.sendDelete = function(){
-        $http.delete($scope.url).then(function(response){
-            var res = JSON.stringify(response.data,null,2);
+    $scope.sendDelete = function() {
+        $http.delete($scope.url).then(function(response) {
+            var res = JSON.stringify(response.data, null, 2);
             $scope.dataResponse = res;
             $scope.stateCode = response.status;
-        }, function (response) {
-            $scope.dataResponse=response.status+", "+response.statusText;
+        }, function(response) {
+            $scope.dataResponse = response.status + ", " + response.statusText;
             $scope.stateCode = response.status;
         });
     };
